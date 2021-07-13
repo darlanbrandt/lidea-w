8266a3b7668fd370db5fcedfefe4c1e1f5a31063
darlanbrandt <darlanbrandt@gmail.com>
2021-07-13 17:36:34 -0300

    Adjust element name in Main Content

--- src/components/page/Main.js
+++ src/components/page/Main.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import css from './main.module.css';
 import device from '../../assets/images/DeviceSmall.png';
-import PageTitle from '../ai_components/ScreenTitle';
+import ScreenTitle from '../ai_components/ScreenTitle';
 
 export default function Main() {
   return (
     <div className={css.main}>
       <div className={css.device}>
         <div className={css.phoneArea}>
-          <PageTitle />
+          <ScreenTitle />
           Components go here
           <br />
           <br />
