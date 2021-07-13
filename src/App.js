ad20cc9353a5c768ee0d693e4b6697dfca54f892
darlanbrandt <darlanbrandt@gmail.com>
2021-07-12 23:28:13 -0300

    Adjust application visuals

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/page/Header';
+import Main from './components/page/Main';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 }
