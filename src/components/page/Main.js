cf7dbbe1fb47a0e9255e47ee928c8bb3d4f09929
darlanbrandt <darlanbrandt@gmail.com>
2021-07-13 16:31:55 -0300

    Add components, placeholder image and adjust main content CSS

--- src/components/page/Main.js
+++ src/components/page/Main.js
@@ -6,7 +6,83 @@ export default function Main() {
   return (
     <div className={css.main}>
       <div className={css.device}>
-        <div className={css.phoneArea}>Components go here</div>
+        <div className={css.phoneArea}>
+          Components go here
+          <br />
+          <br />
+          Components go here
+          <br />
+          <br />
+          Components go here
+          <br />
+          <br />
+          Components go here
+          <br />
+          <br />
+          Components go here
+          <br />
+          <br />
+          Components go here
+          <br />
+          <br />
+          Components go here
+          <br />
+          <br />
+          Components go here
+          <br />
+          <br />
+          Components go here
+          <br />
+          <br />
+          Components go here
+          <br />
+          <br />
+          Components go here
+          <br />
+          <br />
+          Components go here
+          <br />
+          <br />
+          Components go here
+          <br />
+          <br />
+          Components go here
+          <br />
+          <br />
+          Components go here
+          <br />
+          <br />
+          Components go here
+          <br />
+          <br />
+          Components go here
+          <br />
+          <br />
+          Components go here
+          <br />
+          <br />
+          Components go here
+          <br />
+          <br />
+          Components go here
+          <br />
+          <br />
+          Components go here
+          <br />
+          <br />
+          Components go here
+          <br />
+          <br />
+          Components go here
+          <br />
+          <br />
+          Components go here
+          <br />
+          <br />
+          Components go here
+          <br />
+          <br />
+        </div>
         <img src={device} alt="device" />
       </div>
     </div>
