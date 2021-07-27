f1d74fc2e2de835052c0c78a6629bd986104ff49
darlanbrandt <darlanbrandt@gmail.com>
2021-07-27 05:30:41 -0300

    Adjust Commands to execute

--- src/helpers/commandsToExecuteHelper.js
+++ src/helpers/commandsToExecuteHelper.js
@@ -4,16 +4,27 @@ const commandToExecute = (commands) => {
   commands.forEach((command) => {
     if (command.commandType === 'set-property') {
       let componentId = '#' + command.componentAction;
-
+      let value = '';
+      console.log(command.propertyValue);
+      if (typeof command.propertyValue === 'object') {
+        if ('component' in command.propertyValue) {
+          let componentValueId = '#' + command.propertyValue.component;
+          value = document.querySelector(componentValueId).innerHTML;
+        } else if ('scopeVariable' in command.propertyValue) {
+          console.log(command.propertyValue.scopeVariable);
+        }
+      } else {
+        value = command.propertyValue;
+      }
+      console.log(value);
       switch (command.propertyAction) {
         case 'BackgroundColor':
-          let backgroundColor =
-            '#' + convertDecimaltoHexColor(command.propertyValue);
+          let backgroundColor = '#' + convertDecimaltoHexColor(value);
           document.querySelector(componentId).style.backgroundColor =
             backgroundColor;
           break;
         case 'Text':
-          document.querySelector(componentId).innerHTML = command.propertyValue;
+          document.querySelector(componentId).innerHTML = value;
       }
     } else if (command.commandType === 'set-global-var-value') {
       console.log('variavel global');
