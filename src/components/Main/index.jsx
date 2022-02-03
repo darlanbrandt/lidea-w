cbe157dd67bcfedd22eec0080f3da16bd8d857ef
darlanbrandt <darlanbrandt@gmail.com>
2022-02-03 18:25:47 -0300

    Fix position of items

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ElementsContainer from '../ElementsContainer';
 import style from './main.module.css';
+import ScreenTitle from '../ai_components/ScreenTitle';
 
 export default function Main() {
   return (
@@ -8,6 +9,7 @@ export default function Main() {
       <div className={style['phone-area']}>
         <div className={style.device}>
           <div className={style['device-screen']}>
+            <ScreenTitle />
             <ElementsContainer />
           </div>
         </div>
